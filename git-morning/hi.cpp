fc2fde4c4af9021541923a27d9604628653651f3
Sobothty <supercrisz24@gmail.com>
2025-06-20 09:32:32 +0700

    sjdhjsfds

--- git-morning/hi.cpp
+++ git-morning/hi.cpp
@@ -6,5 +6,8 @@ int main()
 {
     cout << "Hello, Git!" << endl;
     cout << "bong slanh oun." << endl;
+    cout << "Git morning!" << endl;
+    cout << "Git is a version control system." << endl;
+    cout << "Git is used to track changes in source code." << endl;
     return 0;
 }
